80e01ab09d7d359e73200056302a5bdf8364a744
JohnDorian00 <remailfunny@gmail.com>
2023-06-22 21:15:37 +0300

    c

---
+++ libs/lib1/lib2/file1.py
@@ -0,0 +1,3 @@
+# какой то код, который что-то делает
+def func():
+    print('func_from_lib1-lib2_file1')
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import libs.outter_lib as outter
 import libs.lib1.file1 as lib1_file1
 import libs.lib1.file2 as lib1_file2
+import libs.lib1.lib2.file1 as lib1_2
 
 outter.func1_file1(6, 9)
 lib1_file1.func()
 lib1_file2.func()
+lib1_2.func()
