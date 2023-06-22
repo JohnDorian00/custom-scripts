5f8a997760bf7ff8bf357a12a8893b1e4b0120a1
JohnDorian00 <remailfunny@gmail.com>
2023-06-22 23:18:46 +0300

    с

--- example.py
+++ example.py
@@ -1,6 +1,10 @@
-class Example:
+# Класс с функциями
+class ExampleClass:
+    # Это конструктор, сюда передаем объект драйвер, когда создаем наш объект в main.py
     def __init__(self, driver=None):
         self.driver = driver
 
     def func(self):
+        # тут self.driver тоже самое, что driver в main.py (ну мы его и передали при создании объекта)
+        # есть доступ ко всем полям и методам объекта
         return self.driver.get_name()
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from example import Example
+from example import ExampleClass
 
 
+# сюды не смотри, тут просто симуляция класса драйвер
+# как я понял - у тебя просто есть уже готовый объект driver, который я создаю ниже
 class Driver:
     def __init__(self, name):
         self.name = name
@@ -9,7 +11,12 @@ class Driver:
         return self.name
 
 
+# делаем объект драйвер, проверяем как работает из main
 driver = Driver('driver_name')
+driver.get_name()
 
-my_object = Example(driver)
-print(my_object.func())
+# создаем объект класса ExampleClass, через этот объект есть доступ ко всем функциям
+my_object_with_functions = ExampleClass(driver)
+
+# юзаем функцию, выводим результат
+print(my_object_with_functions.func())
