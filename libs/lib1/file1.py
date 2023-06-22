7807d799b0085d8c620cf8aecca00be5951ba7a7
JohnDorian00 <remailfunny@gmail.com>
2023-06-22 21:03:25 +0300

    Initial commit

---
+++ libs/lib1/file1.py
@@ -0,0 +1,3 @@
+# какой то код, который что-то делает
+def func():
+    print('func_from_lib1_file1')
---
+++ main.py
@@ -0,0 +1,7 @@
+import libs.outter_lib as outter
+import libs.lib1.file1 as lib1_file1
+import libs.lib1.file2 as lib1_file2
+
+outter.func1_file1(6, 9)
+lib1_file1.func()
+lib1_file2.func()
